956e7c48ef53bd2a5d90284310cb372ba3697aa3
Santiago Zaidan <santyagozaidan@gmail.com>
2022-12-02 15:51:56 -0300

    Project finished

--- script.js
+++ script.js
@@ -1,25 +1,8 @@
-import checkComplete from './components/checkComplete.js';
-import deleteIcon from './components/deleteIcon.js';
+import { addTask } from "./components/addTask.js";
+import { readTask } from "./components/readTasks.js";
 
 const btn = document.querySelector('[data-form-btn]');
 
-const createTask = (e) => {
-  const input = document.querySelector('[data-form-input]');
-  const value = input.value;
-  input.value = '';
-  e.preventDefault();
-  const list = document.querySelector('[data-list]');
-  const task = document.createElement('li');
-  task.classList.add('card');
-  const taskContent = document.createElement('div');
-  const titleTask = document.createElement('span');
-  titleTask.classList.add('task');
-  titleTask.innerText = value;
-  taskContent.appendChild(checkComplete());
-  taskContent.appendChild(titleTask);
-  task.appendChild(taskContent);
-  task.appendChild(deleteIcon());
-  list.appendChild(task);
-};
+btn.addEventListener('click', addTask);
 
-btn.addEventListener('click', createTask);
+readTask();
